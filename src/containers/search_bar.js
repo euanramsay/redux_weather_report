28cc3868c30d52c49cd9a65676318494e1c9b143
Euan Ramsay <euancramsay@aol.com>
2017-02-20 20:14:07 +0000

    making API request

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,20 +1,37 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
 
-export default class SearchBar extends Component {
+class SearchBar extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = { term: '' };
+
+		this.onInputChange = this.onInputChange.bind(this);
+		this.onFormSubmit = this.onFormSubmit.bind(this);
+	}
+
+	onInputChange(event) {
+		this.setState({ term: event.target.value })
+	}
+
+	onFormSubmit(event) {
+		event.preventDefault();
+
+		this.props.fetchWeather(this.state.term);
+		this.setState({ term: '' });
 	}
 
 	render() {
 		return (
-			<form className="input-group">
+			<form onSubmit={this.onFormSubmit} className="input-group">
 				<input 
 					placeholder="Get a five-day forcast in your favorite cities"
 					className="form-control"
 					value={this.state.term}
-					onChange={this.onImputChange} />
+					onChange={this.onInputChange} />
 				<span className="input-group-btn">
 					<button type="submit" className="btn btn-secondary">Submit</button>
 				</span>
@@ -23,3 +40,11 @@ export default class SearchBar extends Component {
 	}
 
 }
+
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(SearchBar);
+
+
